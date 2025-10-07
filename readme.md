c58f8830b8feece8a61e519354cc12a8fc431dbd
zljin <464480515@qq.com>
2025-10-07 12:46:02 +0800

    @zljin 添加postman脚本和搭建swagger文档

--- readme.md
+++ readme.md
@@ -1,1 +1,16 @@
 # flashbuy
+> 技术栈:JDK17+SprintBoot3+MyBatisPlus+MySQL+Redis
+
+> 如何启动
+
+1. 打开src/doc目录，导入flashbuy.sql到你的数据库中
+2. 修改application.yml中的数据库和redis配置
+3. 运行FlashbuyApplication启动类时，添加下面的启动参数
+```
+spring.profiles.active=dev;app.adminAccount=464480515@qq.com;jwt.secret=mySecretKeyForJwtTokenGenerationInSpringBoot3Application
+```
+4. 打开flashbuy.postman_collection.json 导入postman
+5. 运行postman中的注册一个用户作为admin,然后测试登录等接口,后续需要登录获取jwt token,注册一个admin接口和上面的启动参数app.adminAccount一致
+6. 之后你就能跑通所有postman接口，实现一个秒杀系统的最小实现
+
+> 接口文档: http://localhost:8090/swagger-ui/index.html
--- src/main/java/com/zljin/flashbuy/config/WebMvcConfig.java
+++ src/main/java/com/zljin/flashbuy/config/WebMvcConfig.java
@@ -4,8 +4,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    private static final List<String> businessApiWhiteList = Arrays.asList(
+            "/**/get-otp/**",
+            "/**/register/**",
+            "/**/login/**");
+
+    private static final List<String> swaggerUIWhiteList = Arrays.asList(
+            "/swagger-ui.html",
+            "/swagger-ui/**",
+            "/v3/api-docs",
+            "/v3/api-docs/**",
+            "/webjars/**");
+
 
     private final UserInterceptor userInterceptor;
 
@@ -21,9 +37,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addInterceptor(userInterceptor)
                 .addPathPatterns("/**")
                 //添加白名单
-                .excludePathPatterns("/**/get-otp/**")
-                .excludePathPatterns("/**/register/**")
-                .excludePathPatterns("/**/login/**")
-                .excludePathPatterns("/swagger-resources/**", "/webjars/**", "/v2/**", "/swagger-ui.html/**");
+                .excludePathPatterns(businessApiWhiteList)
+                .excludePathPatterns(swaggerUIWhiteList);
     }
 }
